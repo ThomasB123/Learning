111f1512cef1249dd52258cc93967523cfd38aee
ThomasB123 <44202676+ThomasB123@users.noreply.github.com>
2021-02-14 00:37:47 +0000

    Pong rainbow

--- rl/gravitar-code.py
+++ rl/gravitar-code.py
@@ -30,21 +30,16 @@ import torch.optim as optim
 
 import math
 import torch.autograd as autograd
-#from torch.autograd import Variable
 from IPython.display import clear_output
 import matplotlib.pyplot as plt
 from torch.nn.parameter import Parameter
-#from common.wrappers import make_atari, wrap_deepmind, wrap_pytorch
+from collections import deque
+from gym import spaces
+import cv2
+cv2.ocl.setUseOpenCL(False)
+
 USE_CUDA = torch.cuda.is_available()
 Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
-
-# hyperparameters
-learning_rate = 0.001 #0.0005 # 0.001 in Rainbow
-gamma         = 0.99 #0.98 # 0.99 in Rainbow
-buffer_limit  = 10000 #50000 # 10000 in Rainbow
-batch_size    = 32 # 32 in Rainbow
-video_every   = 25
-print_every   = 5
 
 class NoisyLinear(nn.Module):
     def __init__(self, in_features, out_features, use_cuda, std_init=0.4):
@@ -192,31 +187,33 @@ class ReplayBuffer(object):
         idxes = [random.randint(0, len(self._storage) - 1) for _ in range(batch_size)]
         return self._encode_sample(idxes)
 
-
-env_id = 'CartPole-v0'
-env = gym.make(env_id)
-
 # Rainbow: Combining Improvements in Deep Reinforcement Learning
 # code adapted from https://github.com/higgsfield/RL-Adventure/blob/master/7.rainbow%20dqn.ipynb
 
-class RainbowDQN(nn.Module):
-    def __init__(self, num_inputs, num_actions, num_atoms, Vmin, Vmax):
-        super(RainbowDQN, self).__init__()
+class RainbowCnnDQN(nn.Module):
+    def __init__(self, input_shape, num_actions, num_atoms, Vmin, Vmax):
+        super(RainbowCnnDQN, self).__init__()
 
-        self.num_inputs = num_inputs
+        self.input_shape = input_shape
         self.num_actions = num_actions
         self.num_atoms = num_atoms
-        self.Vmin = Vmin # maybe not needed for Gravitar, but this is for cartpole
-        self.Vmax = Vmax # maybe not needed for Gravitar, but this is for cartpole
+        self.Vmin = Vmin
+        self.Vmax = Vmax
 
-        self.linear1 = nn.Linear(num_inputs, 32)
-        self.linear2 = nn.Linear(32, 64)
+        self.features = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
 
-        self.noisy_value1 = NoisyLinear(64, 64, use_cuda=USE_CUDA)
-        self.noisy_value2 = NoisyLinear(64, self.num_atoms, use_cuda=USE_CUDA)
+        self.noisy_value1 = NoisyLinear(self.feature_size(), 512, use_cuda=USE_CUDA)
+        self.noisy_value2 = NoisyLinear(512, self.num_atoms, use_cuda=USE_CUDA)
 
-        self.noisy_advantage1 = NoisyLinear(64, 64, use_cuda=USE_CUDA)
-        self.noisy_advantage2 = NoisyLinear(64, self.num_atoms * self.num_actions, use_cuda=USE_CUDA)
+        self.noisy_advantage1 = NoisyLinear(self.feature_size(), 512, use_cuda=USE_CUDA)
+        self.noisy_advantage2 = NoisyLinear(512, self.num_atoms * self.num_actions, use_cuda=USE_CUDA)
         
         #self.fc1 = nn.Linear(np.array(env.observation_space.shape).prod(), 256) # was here before
         #self.fc2 = nn.Linear(256, 84)
@@ -224,14 +221,19 @@ class RainbowDQN(nn.Module):
 
     def forward(self, x):
         batch_size = x.size(0)
-        x = F.relu(self.linear1(x))
-        x = F.relu(self.linear2(x))
+        x = x / 255.
+        x = self.features(x)
+        x = x.view(batch_size, -1)
+
         value = F.relu(self.noisy_value1(x))
         value = self.noisy_value2(value)
+
         advantage = F.relu(self.noisy_advantage1(x))
         advantage = self.noisy_advantage2(advantage)
+
         value = value.view(batch_size, 1, self.num_atoms)
         advantage = advantage.view(batch_size, self.num_actions, self.num_atoms)
+
         x = value + advantage - advantage.mean(1, keepdim=True)
         x = F.softmax(x.view(-1, self.num_atoms)).view(-1, self.num_actions, self.num_atoms)
 
@@ -247,8 +249,11 @@ class RainbowDQN(nn.Module):
         self.noisy_advantage1.reset_noise()
         self.noisy_advantage2.reset_noise()
     
+    def feature_size(self):
+        return self.features(autograd.Variable(torch.zeros(1, *self.input_shape))).view(1, -1).size(1)
+    
     def act(self, state):
-        state = Variable(torch.FloatTensor(state).unsqueeze(0))#, volatile=True)
+        state = Variable(torch.FloatTensor(np.float32(state)).unsqueeze(0), volatile=True)
         dist = self.forward(state).data.cpu()
         dist = dist * torch.linspace(self.Vmin, self.Vmax, self.num_atoms)
         action = dist.sum(2).max(1)[1].numpy()[0]
@@ -263,24 +268,8 @@ class RainbowDQN(nn.Module):
             return out.argmax().item()
     '''
 
-num_atoms = 51
-Vmin = -10
-Vmax = 10
-
-current_model = RainbowDQN(env.observation_space.shape[0], env.action_space.n, num_atoms, Vmin, Vmax)
-target_model = RainbowDQN(env.observation_space.shape[0], env.action_space.n, num_atoms, Vmin, Vmax)
-
-if USE_CUDA:
-    current_model = current_model.cuda()
-    target_model = target_model.cuda()
-
-optimiser = optim.Adam(current_model.parameters(), learning_rate)
-
-replay_buffer = ReplayBuffer(buffer_limit)
-
 def update_target(current_model, target_model):
     target_model.load_state_dict(current_model.state_dict())
-update_target(current_model, target_model)
 
 def projection_distribution(next_state, rewards, dones):
     batch_size = next_state.size(0)
@@ -349,9 +338,284 @@ def plot(frame_idx, rewards, losses):
     plt.plot(losses)
     plt.show()
 
+class NoopResetEnv(gym.Wrapper):
+    def __init__(self, env, noop_max=30):
+        """Sample initial states by taking random number of no-ops on reset.
+        No-op is assumed to be action 0.
+        """
+        gym.Wrapper.__init__(self, env)
+        self.noop_max = noop_max
+        self.override_num_noops = None
+        self.noop_action = 0
+        assert env.unwrapped.get_action_meanings()[0] == 'NOOP'
+
+    def reset(self, **kwargs):
+        """ Do no-op action for a number of steps in [1, noop_max]."""
+        self.env.reset(**kwargs)
+        if self.override_num_noops is not None:
+            noops = self.override_num_noops
+        else:
+            noops = self.unwrapped.np_random.randint(1, self.noop_max + 1) #pylint: disable=E1101
+        assert noops > 0
+        obs = None
+        for _ in range(noops):
+            obs, _, done, _ = self.env.step(self.noop_action)
+            if done:
+                obs = self.env.reset(**kwargs)
+        return obs
+
+    def step(self, ac):
+        return self.env.step(ac)
+
+class FireResetEnv(gym.Wrapper):
+    def __init__(self, env):
+        """Take action on reset for environments that are fixed until firing."""
+        gym.Wrapper.__init__(self, env)
+        assert env.unwrapped.get_action_meanings()[1] == 'FIRE'
+        assert len(env.unwrapped.get_action_meanings()) >= 3
+
+    def reset(self, **kwargs):
+        self.env.reset(**kwargs)
+        obs, _, done, _ = self.env.step(1)
+        if done:
+            self.env.reset(**kwargs)
+        obs, _, done, _ = self.env.step(2)
+        if done:
+            self.env.reset(**kwargs)
+        return obs
+
+    def step(self, ac):
+        return self.env.step(ac)
+
+class EpisodicLifeEnv(gym.Wrapper):
+    def __init__(self, env):
+        """Make end-of-life == end-of-episode, but only reset on true game over.
+        Done by DeepMind for the DQN and co. since it helps value estimation.
+        """
+        gym.Wrapper.__init__(self, env)
+        self.lives = 0
+        self.was_real_done  = True
+
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        self.was_real_done = done
+        # check current lives, make loss of life terminal,
+        # then update lives to handle bonus lives
+        lives = self.env.unwrapped.ale.lives()
+        if lives < self.lives and lives > 0:
+            # for Qbert sometimes we stay in lives == 0 condtion for a few frames
+            # so its important to keep lives > 0, so that we only reset once
+            # the environment advertises done.
+            done = True
+        self.lives = lives
+        return obs, reward, done, info
+
+    def reset(self, **kwargs):
+        """Reset only when lives are exhausted.
+        This way all states are still reachable even though lives are episodic,
+        and the learner need not know about any of this behind-the-scenes.
+        """
+        if self.was_real_done:
+            obs = self.env.reset(**kwargs)
+        else:
+            # no-op step to advance from terminal/lost life state
+            obs, _, _, _ = self.env.step(0)
+        self.lives = self.env.unwrapped.ale.lives()
+        return obs
+
+class MaxAndSkipEnv(gym.Wrapper):
+    def __init__(self, env, skip=4):
+        """Return only every `skip`-th frame"""
+        gym.Wrapper.__init__(self, env)
+        # most recent raw observations (for max pooling across time steps)
+        self._obs_buffer = np.zeros((2,)+env.observation_space.shape, dtype=np.uint8)
+        self._skip       = skip
+
+    def reset(self):
+        return self.env.reset()
+
+    def step(self, action):
+        """Repeat action, sum reward, and max over last observations."""
+        total_reward = 0.0
+        done = None
+        for i in range(self._skip):
+            obs, reward, done, info = self.env.step(action)
+            if i == self._skip - 2: self._obs_buffer[0] = obs
+            if i == self._skip - 1: self._obs_buffer[1] = obs
+            total_reward += reward
+            if done:
+                break
+        # Note that the observation on the done=True frame
+        # doesn't matter
+        max_frame = self._obs_buffer.max(axis=0)
+
+        return max_frame, total_reward, done, info
+
+    def reset(self, **kwargs):
+        return self.env.reset(**kwargs)
+
+class ClipRewardEnv(gym.RewardWrapper):
+    def __init__(self, env):
+        gym.RewardWrapper.__init__(self, env)
+
+    def reward(self, reward):
+        """Bin reward to {+1, 0, -1} by its sign."""
+        return np.sign(reward)
+
+class WarpFrame(gym.ObservationWrapper):
+    def __init__(self, env):
+        """Warp frames to 84x84 as done in the Nature paper and later work."""
+        gym.ObservationWrapper.__init__(self, env)
+        self.width = 84
+        self.height = 84
+        self.observation_space = spaces.Box(low=0, high=255,
+            shape=(self.height, self.width, 1), dtype=np.uint8)
+
+    def observation(self, frame):
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        frame = cv2.resize(frame, (self.width, self.height), interpolation=cv2.INTER_AREA)
+        return frame[:, :, None]
+
+class FrameStack(gym.Wrapper):
+    def __init__(self, env, k):
+        """Stack k last frames.
+        Returns lazy array, which is much more memory efficient.
+        See Also
+        --------
+        baselines.common.atari_wrappers.LazyFrames
+        """
+        gym.Wrapper.__init__(self, env)
+        self.k = k
+        self.frames = deque([], maxlen=k)
+        shp = env.observation_space.shape
+        self.observation_space = spaces.Box(low=0, high=255, shape=(shp[0], shp[1], shp[2] * k), dtype=np.uint8)
+
+    def reset(self):
+        ob = self.env.reset()
+        for _ in range(self.k):
+            self.frames.append(ob)
+        return self._get_ob()
+
+    def step(self, action):
+        ob, reward, done, info = self.env.step(action)
+        self.frames.append(ob)
+        return self._get_ob(), reward, done, info
+
+    def _get_ob(self):
+        assert len(self.frames) == self.k
+        return LazyFrames(list(self.frames))
+
+class ScaledFloatFrame(gym.ObservationWrapper):
+    def __init__(self, env):
+        gym.ObservationWrapper.__init__(self, env)
+
+    def observation(self, observation):
+        # careful! This undoes the memory optimization, use
+        # with smaller replay buffers only.
+        return np.array(observation).astype(np.float32) / 255.0
+
+class LazyFrames(object):
+    def __init__(self, frames):
+        """This object ensures that common frames between the observations are only stored once.
+        It exists purely to optimize memory usage which can be huge for DQN's 1M frames replay
+        buffers.
+        This object should only be converted to numpy array before being passed to the model.
+        You'd not believe how complex the previous solution was."""
+        self._frames = frames
+        self._out = None
+
+    def _force(self):
+        if self._out is None:
+            self._out = np.concatenate(self._frames, axis=2)
+            self._frames = None
+        return self._out
+
+    def __array__(self, dtype=None):
+        out = self._force()
+        if dtype is not None:
+            out = out.astype(dtype)
+        return out
+
+    def __len__(self):
+        return len(self._force())
+
+    def __getitem__(self, i):
+        return self._force()[i]
+
+def make_atari(env_id):
+    env = gym.make(env_id)
+    assert 'NoFrameskip' in env.spec.id
+    env = NoopResetEnv(env, noop_max=30)
+    env = MaxAndSkipEnv(env, skip=4)
+    return env
+
+def wrap_deepmind(env, episode_life=True, clip_rewards=True, frame_stack=False, scale=False):
+    """Configure environment for DeepMind-style Atari.
+    """
+    if episode_life:
+        env = EpisodicLifeEnv(env)
+    if 'FIRE' in env.unwrapped.get_action_meanings():
+        env = FireResetEnv(env)
+    env = WarpFrame(env)
+    if scale:
+        env = ScaledFloatFrame(env)
+    if clip_rewards:
+        env = ClipRewardEnv(env)
+    if frame_stack:
+        env = FrameStack(env, 4)
+    return env
+
+class ImageToPyTorch(gym.ObservationWrapper):
+    """
+    Image shape to num_channels x weight x height
+    """
+    def __init__(self, env):
+        super(ImageToPyTorch, self).__init__(env)
+        old_shape = self.observation_space.shape
+        self.observation_space = gym.spaces.Box(low=0.0, high=1.0, shape=(old_shape[-1], old_shape[0], old_shape[1]), dtype=np.uint8)
+
+    def observation(self, observation):
+        return np.swapaxes(observation, 2, 0)
+    
+def wrap_pytorch(env):
+    return ImageToPyTorch(env)
+
+
+# hyperparameters
+learning_rate = 0.0001 #0.0005 # 0.001 in Rainbow
+gamma         = 0.99 #0.98 # 0.99 in Rainbow
+buffer_limit  = 100000 #50000 # 10000 in Rainbow
+batch_size    = 32 # 32 in Rainbow
+video_every   = 25
+print_every   = 5
+
+#env_id = 'CartPole-v0'
+env_id = 'PongNoFrameskip-v4'
+env = make_atari(env_id)
+env = wrap_deepmind(env)
+env = wrap_pytorch(env)
+
+num_atoms = 51
+Vmin = -10
+Vmax = 10
+
+current_model = RainbowCnnDQN(env.observation_space.shape, env.action_space.n, num_atoms, Vmin, Vmax)
+target_model = RainbowCnnDQN(env.observation_space.shape, env.action_space.n, num_atoms, Vmin, Vmax)
+
+if USE_CUDA:
+    current_model = current_model.cuda()
+    target_model = target_model.cuda()
+
+optimiser = optim.Adam(current_model.parameters(), lr=learning_rate)
+update_target(current_model, target_model)
+
+replay_initial = 10000
+replay_buffer = ReplayBuffer(buffer_limit)
+
+
 # Training:
 
-num_frames = 15000
+num_frames = 1000000
 
 losses = []
 all_rewards = []
@@ -360,22 +624,30 @@ episode_reward = 0
 state = env.reset()
 for frame_idx in range(1, num_frames+1):
     action = current_model.act(state)
+
     next_state, reward, done, _ = env.step(action)
     replay_buffer.push(state, action, reward, next_state, done)
+
     state = next_state
     episode_reward += reward
+
     if done:
         state = env.reset()
         all_rewards.append(episode_reward)
         episode_reward = 0
-    if len(replay_buffer) > batch_size:
+
+    if len(replay_buffer) > replay_initial:
         loss = compute_td_loss(batch_size)
-        losses.append(loss.data.item())#[0])
-    if frame_idx % 200 == 0:
+        losses.append(loss.data)
+
+    if frame_idx % 10000 == 0:
         plot(frame_idx, all_rewards, losses)
+
     if frame_idx % 1000 == 0:
         update_target(current_model, target_model)
 
+
+'''
 def train(q, q_target, memory, optimizer):
     for i in range(10):
         s,a,r,s_prime,done_mask = memory.sample(batch_size)
@@ -389,6 +661,7 @@ def train(q, q_target, memory, optimizer):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+'''
 
 """**Train**
 
@@ -397,20 +670,9 @@ def train(q, q_target, memory, optimizer):
 
 # setup the Gravitar ram environment, and record a video every 50 episodes. You can use the non-ram version here if you prefer
 # Gravitar-ram-v0
-# Gravitar-ram-v4
-# Gravitar-ramDeterministic-v0
-# Gravitar-ramDeterministic-v4
-# Gravitar-ramNoFrameskip-v0
-# Gravitar-ramNoFrameskip-v4
 # Gravitar-v0
-# Gravitar-v4
-# Gravitar-Deterministic-v0
-# Gravitar-Deterministic-v4
-# GravitarNoFrameskip-v0
-# GravitarNoFrameskip-v4
 
 #env = gym.make('Gravitar-ram-v0')
-#env = gym.make('PongNoFrameskip-v4')
 #env = gym.wrappers.Monitor(env, "./video", video_callable=lambda episode_id: (episode_id%video_every)==0,force=True)
 '''
 # reproducible environment and action spaces, do not change lines 6-11 here (tools > settings > editor > show line numbers)
